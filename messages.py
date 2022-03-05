a4dfdd95f6ac5c18547e46f95e9e0399691b9efb
DeeCaaD <perttu.kangas@hotmail.fi>
2022-03-05 16:53:14 +0200

    Added find messages function

--- messages.py
+++ messages.py
@@ -3,6 +3,7 @@ from flask import render_template, request, redirect
 from db import db
 import users
 
+
 # ROUTING START
 
 
@@ -17,7 +18,6 @@ def thread(section_id, thread_id):
 
 @app.route("/new_message", methods=["post"])
 def new_message():
-
     if not users.is_logged_in():
         return render_template("error.html", message="Täytyy olla kirjautunut sisään lähettääkseen viestin")
 
@@ -81,6 +81,20 @@ def edit_message():
 
     return redirect("/section/" + str(section_id) + "/thread/" + str(thread_id))
 
+
+@app.route("/find_message")
+def find_message():
+    message = request.form["message"]
+    if len(message) < 3 or len(message) > 20:
+        return render_template("error.html", message="Viestin etsinnän tulee olla 3-20 merkkiä")
+
+    messages = sql_find_messages(message)
+    if messages is None:
+        return render_template("error.html", message="Hakusanalla " + message + " ei löytynt viestejä.")
+
+    return render_template("find.html", messages=messages)
+
+
 # ROUTING END
 
 
@@ -113,9 +127,24 @@ def sql_new_message(thread_id, sender_id, message):
 def sql_get_messages(thread_id):
     user_id = users.user_id()
     user_role = users.user_role()
-    sql = "SELECT U.username, M.sent_at, M.message, M.id, (M.sender_id=:user_id OR :user_role>0) " \
-          "FROM users U, messages M WHERE M.thread_id=:thread_id AND U.id=M.sender_id " \
+    sql = "SELECT U.username, M.sent_at, M.message, M.id, (M.sender_id=:user_id OR :user_role>0), T.name " \
+          "FROM users U, messages M, threads T WHERE M.thread_id=:thread_id AND U.id=M.sender_id AND T.id=:thread_id " \
           "ORDER BY M.id"
 
     result = db.session.execute(sql, {"thread_id": thread_id, "user_id": user_id, "user_role": user_role})
+    return result.fetchall()
+
+
+def sql_find_messages(message):
+    user_id = users.user_id()
+    user_role = users.user_role()
+
+    sql = "SELECT S.id, S.name, T.id, T.name, M.id, M.message, M.sent_at " \
+          "FROM sections S, sections_access SA, threads T, messages M " \
+          "WHERE M.message LIKE :message AND M.thread_id=T.id AND T.section_id=S.id " \
+          "AND (S.hidden=0 OR :user_role=1 OR :user_id" \
+          " IN (SELECT SA.user_id FROM sections_access SA WHERE SA.section_id=S.id))" \
+          "ORDER BY M.sent_at"
+
+    result = db.session.execute(sql, {"message": "%" + message + "%", "user_id": user_id, "user_role": user_role})
     return result.fetchall()
